413832c017361cc2ebf18bad594d2c3efd55b047
CALDREC-UST <157812419+caldrecust@users.noreply.github.com>
2024-01-26 22:33:30 +0800

    Examples .m

---
+++ Examples/Design_Columns_Ex01.m
@@ -0,0 +1,84 @@
+% Design_Columns_Ex01
+%----------------------------------------------------------------
+% PURPOSE 
+%    To design optimally (with respect to saving in reinforcing area)
+%    a rectangular column' cross-section, considering biaxial bending
+%    loads - three options of reinforcement optimization are
+%    possible: with a pure ISR, symmtrical rebar or asymmetrical rebar
+%
+%    Note: function "isrColumnsSymAsym" is the only on required.
+%
+%          function "ExportResultsColumn" is the one required to export the
+%          results to given disc direction through the parameter 
+%          "directionData" as a string variable
+%
+%----------------------------------------------------------------
+
+% LAST MODIFIED: L.F.Veduzco    2023-07-03
+% Copyright (c)  School of Engineering
+%                The Hong Kong University of Science and Technology (HKUST)
+%----------------------------------------------------------------
+
+clear all
+clc
+
+%% GEOMETRY
+height=300; % column's length
+b=30; % cross-section width
+h=40; % cross-section height
+
+%% MATERIALS
+fy=4200; % yield stress of rebars 
+fc=250; % concrete's compressive strength Kg/cm2
+ws=0.0078; % volumetric weight of reinforcing steel (Kg/cm3)
+
+%% Loads
+load_conditions=[1 -53.52e3 -9.602e5 10e5]; % [nload, Pu, Mx, My]
+      
+%% Rebar data
+% Commerically available rebars
+RebarAvailable=[4 4/8*2.54;
+                5 5/8*2.54;
+                6 6/8*2.54;
+                8 8/8*2.54;
+                9 9/8*2.54;
+                10 10/8*2.54;
+                12 12/8*2.54];
+%% ADITIONAL PARAMETERS
+rec=[4 4]; % [coverx covery] - concrete cover
+
+cols_sym_asym_isr="Asymmetric";
+
+pu_cols=[28.93]./18;
+puColBuild=[1.2,0.9,128,214.75,0.04,0.105,7];
+
+condition_cracking="Cracked"; % "Cracked" or "Non-cracked"
+dataCFA=[0,1,1,2]; % Data required for the computation of the 
+                   % constructability factor for the rebar designs of columns
+                   % Lower and upper values for the range of the CFA and 
+                   % weight factors for the uniformity of rebars and rebar 
+                   % diameters
+% Plot options: 
+optimaPlot=1; % optima reinforcement area convergence plot
+plotsISRdiagrams=1; % interaction diagrams
+plotRebarDesign=1; % detailed reinforced cross-section
+
+% Ductility demand
+ductility=3;
+             
+%% Optimization process
+[Inertia_xy_modif,b,h,bestArrangement,bestdisposicionRebar,cost_elem_col,...
+Ac_sec_elem,Ef_sec_col,Mr_col,bestCFA]=isrColumnsSymAsym(pu_cols,height,b,h,rec,...
+fy,fc,load_conditions,cols_sym_asym_isr,condition_cracking,ductility,...
+ws,RebarAvailable,puColBuild,dataCFA,optimaPlot,plotsISRdiagrams,plotRebarDesign);
+
+%% Export results
+
+dimColumnsCollection=[b h height rec(1)];
+nbarColumnsCollection=length(bestdisposicionRebar(:,1));
+
+% disc's route to export design results
+directionData='C:\Users\lfver\OneDrive\Desktop\OneDrive\DynamoRevit\Dynamo_visualization\';
+
+ExportResultsColumn(directionData,dimColumnsCollection,bestdisposicionRebar,...
+    nbarColumnsCollection,bestArrangement,cols_sym_asym_isr,[0,0,0]);
---
+++ Examples/Efficiency_Beam_RebarDesign_Ex01.m
@@ -0,0 +1,76 @@
+% Efficiency_Beam_RebarDesign_Ex01
+%----------------------------------------------------------------
+% PURPOSE 
+%    To determine the structural efficiency of a
+%    reinforced rectangular beam cross-section.
+%
+%----------------------------------------------------------------
+% CREATED:       L.F.Veduzco    2023-03-18
+%                Faculty of Engineering
+%                Autonomous University of Queretaro
+%
+% LAST MODIFIED: L.F.Veduzco    2023-07-03
+% Copyright (c)  School of Engineering
+%                The Hong Kong University of Science and Technology (HKUST)
+%----------------------------------------------------------------
+
+clc
+clear all
+
+%% Geometry
+b=15; % cross-section width
+h=30; % cross-section height
+
+b_rec=5; % concrete cover on each direction
+h_rec=5;
+
+d=h-h_rec; % effective cross-section height
+
+%% Materials
+fc=250; % concrete's compressive strength
+factor_fc=0.85; % reduction factor for the f'c
+fdpc=factor_fc*fc; % reduced f'c
+E=2e6; % Modulus of elasticity of the reinforcing steel
+fy=4200; % Yield stress of the reinforcing steel
+
+%% Additional parameters
+duct=3; % ductility demand level
+
+%% Loads
+load_conditions=[1 -0.02e5]; % [n-load, Mu] (Kg-cm)
+
+%% Rebar data
+% Commercially available rebar diameters
+                %type diam  
+rebarAvailable=[3 3/8*2.54;
+                4 4/8*2.54;
+                5 5/8*2.54;
+                6 6/8*2.54;
+                8 8/8*2.54;
+                9 9/8*2.54;
+                10 10/8*2.54;
+                12 12/8*2.54];
+            
+% Distribution of rebars over the cross-section
+dispositionRebar=[-2.5 10;
+                    2.5 10;
+                    -2.5 -10;
+                    2.5 -10];
+                
+RebarIndexTen=[1;1]; % rebar diameters to use for the reinforcement
+                    % in tension (indices from the "rebarAvailable" array)
+                    
+RebarIndexCom=[1;1]; % rebar diameters to use for the reinforcement
+                  % in compression (indices from the "rebarAvailable" array)
+
+%% Additional design information of interest
+ast=sum(rebarAvailable(RebarIndexTen,2).^2.*pi./4);
+
+astotal=ast % Total rebar area
+rho=astotal/(b*d) % Total percentage area
+amax=0.025*b*d % Max allowed rebar area by code
+
+%% Structural efficiency
+[maxef,Mrv,c]=EfcriticalRebarbeams(load_conditions,b,E,fdpc,RebarIndexTen,...
+    RebarIndexCom,rebarAvailable,d,h_rec,0.85,dispositionRebar)
+
